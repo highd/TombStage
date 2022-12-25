021f58b20e443034b3c919726abb3505d8bc5473
drania <hdgames123@yahoo.co.jp>
2022-12-25 20:45:07 +0900

    初期コミット

---
+++ src/main/java/com/highd120/tombstagesmod/TombStagesMod.java
@@ -0,0 +1,17 @@
+package com.highd120.tombstagesmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = TombStagesStatus.MOD_ID, name = TombStagesStatus.NAME, version = TombStagesStatus.VERSION)
+public class TombStagesMod{
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+}
---
+++ src/main/java/com/highd120/tombstagesmod/TombStagesStatus.java
@@ -0,0 +1,7 @@
+package com.highd120.tombstagesmod;
+
+public class TombStagesStatus {
+    public static final String MOD_ID = "tombstagesmod";
+    public static final String NAME = "Tomb Stages";
+    public static final String VERSION = "1.0";
+}
