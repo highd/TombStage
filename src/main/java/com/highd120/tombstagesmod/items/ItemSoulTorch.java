a431e1627f63cf4ba8053e69f9c0ba10e6955b7c
drania <hdgames123@yahoo.co.jp>
2023-01-26 20:49:53 +0900

    魂の杖のON/OFF追加

--- src/main/java/com/highd120/tombstagesmod/items/ItemSoulTorch.java
+++ src/main/java/com/highd120/tombstagesmod/items/ItemSoulTorch.java
@@ -2,9 +2,13 @@ package com.highd120.tombstagesmod.items;
 
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import com.highd120.tombstagesmod.TombStageConfig;
+import com.highd120.tombstagesmod.TombStagesStatus;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
@@ -22,23 +26,34 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemSoulTorch extends ItemBase {
+	String[] meteNameList = new String[] {
+		"soul_less_torch",
+		"soul_torch"
+	};
 	public ItemSoulTorch() {
 		super();
 		setMaxStackSize(1);
 		setCreativeTab(CreativeTabs.MISC);
+		setHasSubtypes(true);
 	}
 
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack stack= playerIn.getHeldItem(handIn);
+		if (stack.getMetadata() == 0) {
+    		return super.onItemRightClick(worldIn, playerIn, handIn);
+		}
     	if (playerIn.getHeldItemMainhand() != stack) {
     		return super.onItemRightClick(worldIn, playerIn, handIn);
     	}
@@ -57,7 +72,11 @@ public class ItemSoulTorch extends ItemBase {
 		}
 		IFluidHandlerItem handler = FluidUtil.getFluidHandler(stack);
 		handler.drain(TombStageConfig.soulTorchConsumption, true);
-    	
+		fluidStack = FluidUtil.getFluidContained(stack);
+		if (fluidStack.amount <= TombStageConfig.soulTorchConsumption) {
+			stack.setItemDamage(1);
+		}
+		
 		double x = tag.getInteger("X");
 		double y = tag.getInteger("Y");
 		double z = tag.getInteger("Z");
@@ -76,6 +95,19 @@ public class ItemSoulTorch extends ItemBase {
 					speed.x, speed.y, speed.z);
 		}
 		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
+	}
+	
+	@Override
+	public int getMetadata(ItemStack stack) {
+		FluidStack fluidStack = FluidUtil.getFluidContained(stack);
+		if (fluidStack == null || fluidStack.amount <= TombStageConfig.soulTorchConsumption) {
+			return 0;
+		}
+    	NBTTagCompound tag = stack.getTagCompound();
+    	if (tag == null || !tag.hasKey("X") ) {
+    		return 0;
+    	}
+		return 1;
 	}
     
     @Override
@@ -101,5 +133,22 @@ public class ItemSoulTorch extends ItemBase {
 				return fluid.getFluid().getName().equals(TombStageConfig.soulTorchFuel);
 			}
 		};
+	}
+	
+	@Nonnull
+	@Override
+	public String getUnlocalizedName(ItemStack par1ItemStack) {
+		return "item." + TombStagesStatus.MOD_ID + "." + meteNameList[Math.min(meteNameList.length - 1,
+				par1ItemStack.getItemDamage())];
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModel() {
+		for (int i = 0; i < meteNameList.length; i++) {
+			String location = TombStagesStatus.MOD_ID + ":" + meteNameList[i];
+			ModelLoader.setCustomModelResourceLocation(this, i,
+					new ModelResourceLocation(location, "inventory"));
+		}
 	}
 }
